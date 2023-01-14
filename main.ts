f59eaf9d1d1dcd0339004f32fdf0f2bcb53a3354
Michal Grňo <m93a.cz@gmail.com>
2023-01-14 23:57:00 +0100

    kaktus stats extraction works now

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
 import { CookieJar, wrapFetch } from "https://deno.land/x/another_cookiejar@v5.0.2/mod.ts";
-import { DOMParser, Element, HTMLDocument } from "https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-wasm.ts";
+import {
+  DOMParser,
+  Element,
+  HTMLDocument,
+  NodeList,
+} from "https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-wasm.ts";
 
 import secrets from "./secrets.json" assert { type: "json" };
 
@@ -7,6 +12,12 @@ import secrets from "./secrets.json" assert { type: "json" };
 const yeet = (err: any): never => {
   throw err;
 };
+
+/** wait for a specified number of milliseconds */
+const delay = (ms: number) => new Promise<void>((res) => setTimeout(res, ms));
+
+const onlyElements = (list: NodeList | undefined): Element[] =>
+  [...(list ?? [])].filter((el): el is Element => el instanceof Element);
 
 async function responseToHtml(res: Response): Promise<HTMLDocument> {
   const parser = new DOMParser();
@@ -26,8 +37,7 @@ async function kaktusLoadLazyIslands(
   cookieJar: CookieJar,
   parentEl: Element
 ): Promise<Array<{ title: string; el: Element }>> {
-  const islandNodes = [...parentEl.querySelectorAll("[data-lazy-loading]")];
-  const islandEls = islandNodes.filter((el): el is Element => el instanceof Element);
+  const islandEls = onlyElements(parentEl.querySelectorAll("[data-lazy-loading]"));
 
   const islands = islandEls.map(kaktusParseIslandElement);
   const awaitedIslands = await Promise.all(
@@ -64,14 +74,56 @@ async function kaktusFetchIsland(
   const loadingEl = islandsDom.querySelector("[data-lazy-loading]");
 
   if (loadingEl) {
+    await delay(50); // wait 50ms before asking again
+
     const { requestId, componentId } = kaktusParseIslandElement(loadingEl);
-    return kaktusFetchIsland(cookieJar, requestId, componentId, attempts - 1);
+    return await kaktusFetchIsland(cookieJar, requestId, componentId, attempts - 1);
   }
 
   return islandsDom.getElementById(componentId)!;
 }
 
-export async function checkMobileData() {
+export interface KaktusStatus {
+  credit: {
+    total: number;
+    standard: {
+      amount: number;
+      expires: Date;
+    };
+    bonus: {
+      amount: number;
+      expires: Date;
+    };
+  };
+
+  tariff: {
+    autoSubscription: boolean;
+    tariffName: string;
+    renewalDate: Date;
+
+    gigsLeft: number;
+    minutesLeft: number;
+    secondsLeft: number;
+    smsLeft: number;
+  };
+}
+
+function findElementByTextContent(parent: Element | undefined, selector: string, pattern: string): Element | undefined {
+  return onlyElements(parent?.querySelectorAll(selector)).find((el) =>
+    el.textContent.toLocaleLowerCase().includes(pattern)
+  );
+}
+
+function parseDate(dateString: string): Date {
+  const [_, day, month, year] = dateString.match(/(\d{1,2}).(\d{1,2}).(\d{4})/) ?? [];
+  return new Date(+year, +month - 1, +day);
+}
+
+function parseDecimal(decimalString: string): number {
+  return parseFloat(decimalString.replace(",", "."));
+}
+
+export async function kaktusCheckStatus(): Promise<KaktusStatus> {
   const cookieJar = new CookieJar();
 
   const fetch = wrapFetch({ cookieJar });
@@ -86,13 +138,51 @@ export async function checkMobileData() {
   const skeletonResponse = await fetch(`https://www.mujkaktus.cz/moje-sluzby`);
   const skeletonDOM = await responseToHtml(skeletonResponse);
 
-  // find the lazy-loaded islands
+  // find the lazy-loaded islands and finish loading
   const portletRoot = skeletonDOM.querySelector(".portlet-body") ?? yeet("Portlet root not found in HTML.");
-
   const islands = await kaktusLoadLazyIslands(cookieJar, portletRoot);
-  console.log(islands.map(({ title, el }) => ({ title, content: el.textContent.trim() })));
 
-  return islands;
+  // extract the stats
+
+  const creditIsland = islands.find(({ title }) => title.includes("kredit"))?.el;
+  const standardCredit = findElementByTextContent(creditIsland, "p.font-size-xs", "standard")?.textContent ?? "";
+  const bonusCredit = findElementByTextContent(creditIsland, "p.font-size-xs", "bonus")?.textContent ?? "";
+
+  const tariffIsland = islands.find(({ title }) => title.toLowerCase().includes("balíč"))?.el;
+  const findLabeledEl = (label: string) =>
+    findElementByTextContent(tariffIsland, "p", label)?.previousElementSibling?.textContent;
+  const data = findLabeledEl("data");
+  const calls = findLabeledEl("minut");
+  const sms = findLabeledEl("sms");
+
+  const name =
+    findElementByTextContent(tariffIsland, "p", "balíček:")?.querySelector("strong");
+  const autoSubscription = !!findElementByTextContent(tariffIsland, "span.badge", "samoobnovující");
+  const renewal = findElementByTextContent(tariffIsland, "p", "obnoví se")?.querySelector("strong");
+
+  return {
+    credit: {
+      total: parseInt(creditIsland?.querySelector(".price")?.textContent!),
+      standard: {
+        amount: parseInt(standardCredit.match(/\d[\d\s]+/)?.[0]!),
+        expires: parseDate(standardCredit),
+      },
+      bonus: {
+        amount: parseInt(bonusCredit.match(/\d[\d\s]+/)?.[0]!),
+        expires: parseDate(bonusCredit),
+      },
+    },
+    tariff: {
+      tariffName: name?.textContent ?? "",
+      autoSubscription,
+      renewalDate: parseDate(renewal?.textContent!),
+
+      gigsLeft: parseDecimal(data!),
+      minutesLeft: parseDecimal(calls!),
+      secondsLeft: parseDecimal(calls?.split(":")[1]!),
+      smsLeft: parseDecimal(sms!),
+    },
+  };
 }
 
-await checkMobileData();
+console.log(await kaktusCheckStatus());
