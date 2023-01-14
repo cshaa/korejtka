cf0d7b06787cf4186a3ff4db39d2ed917b9cb9bc
Michal Grňo <m93a.cz@gmail.com>
2023-01-14 22:48:55 +0100

    almost done

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { CookieJar, wrapFetch } from "https://deno.land/x/another_cookiejar@v5.0.2/mod.ts";
-import { DOMParser, Element } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+import { DOMParser, Element, HTMLDocument } from "https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-wasm.ts";
 
 import secrets from "./secrets.json" assert { type: "json" };
 
@@ -7,6 +7,69 @@ import secrets from "./secrets.json" assert { type: "json" };
 const yeet = (err: any): never => {
   throw err;
 };
+
+async function responseToHtml(res: Response): Promise<HTMLDocument> {
+  const parser = new DOMParser();
+  const dom = parser.parseFromString(await res.text(), "text/html");
+  if (!dom) throw new Error("Failed to parse HTML.");
+  return dom;
+}
+
+function kaktusParseIslandElement(el: Element) {
+  const title = el.previousElementSibling?.textContent ?? "";
+  const requestId = JSON.parse(el.getAttribute("data-lazy-loading") ?? "{}")?.rk;
+  const componentId = el.id;
+  return { title, requestId, componentId };
+}
+
+async function kaktusLoadLazyIslands(
+  cookieJar: CookieJar,
+  parentEl: Element
+): Promise<Array<{ title: string; el: Element }>> {
+  const islandNodes = [...parentEl.querySelectorAll("[data-lazy-loading]")];
+  const islandEls = islandNodes.filter((el): el is Element => el instanceof Element);
+
+  const islands = islandEls.map(kaktusParseIslandElement);
+  const awaitedIslands = await Promise.all(
+    islands.map(({ title, requestId, componentId }) =>
+      kaktusFetchIsland(cookieJar, requestId, componentId).then((el) => ({ title, el }))
+    )
+  );
+
+  return awaitedIslands;
+}
+
+async function kaktusFetchIsland(
+  cookieJar: CookieJar,
+  requestId: string,
+  componentId: string,
+  attempts = 10
+): Promise<Element> {
+  if (attempts <= 0) throw "Could not load lazy-loaded islands in the specified number of attempts.";
+  const fetch = wrapFetch({ cookieJar });
+
+  const lazyLoadingURLs =
+    "https://www.mujkaktus.cz/moje-sluzby" +
+    "?p_p_id=rkaktusvcc_WAR_vcc&p_p_lifecycle=0" +
+    "&p_p_state=exclusive&p_p_mode=view" +
+    "&p_p_col_id=column-1&p_p_col_count=1" +
+    "&_rkaktusvcc_WAR_vcc_moduleCode=dashboard" +
+    "&_rkaktusvcc_WAR_vcc_lazyLoading=true" +
+    "&_rkaktusvcc_WAR_vcc_componentIds=" +
+    `${requestId}.${componentId}`;
+
+  const islandsResponse = await fetch(lazyLoadingURLs);
+  const islandsDom = await responseToHtml(islandsResponse);
+
+  const loadingEl = islandsDom.querySelector("[data-lazy-loading]");
+
+  if (loadingEl) {
+    const { requestId, componentId } = kaktusParseIslandElement(loadingEl);
+    return kaktusFetchIsland(cookieJar, requestId, componentId, attempts - 1);
+  }
+
+  return islandsDom.getElementById(componentId)!;
+}
 
 export async function checkMobileData() {
   const cookieJar = new CookieJar();
@@ -21,38 +84,15 @@ export async function checkMobileData() {
 
   // initiate lazy loading
   const skeletonResponse = await fetch(`https://www.mujkaktus.cz/moje-sluzby`);
-  const skeletonDOM =
-    new DOMParser().parseFromString(await skeletonResponse.text(), "text/html") ?? yeet("Failed to parse HTML.");
+  const skeletonDOM = await responseToHtml(skeletonResponse);
 
   // find the lazy-loaded islands
   const portletRoot = skeletonDOM.querySelector(".portlet-body") ?? yeet("Portlet root not found in HTML.");
-  const islandNodes = [...portletRoot.querySelectorAll("[data-lazy-loading]")];
-  const islandEls = islandNodes.filter((el): el is Element => el instanceof Element);
 
-  const islands = islandEls.map((el) => {
-    const text = el.previousElementSibling?.textContent ?? "";
-    const mainId = JSON.parse(el.getAttribute("data-lazy-loading") ?? "{}")?.rk;
-    const subId = el.id;
-    return { text, mainId, subId };
-  });
+  const islands = await kaktusLoadLazyIslands(cookieJar, portletRoot);
+  console.log(islands.map(({ title, el }) => ({ title, content: el.textContent.trim() })));
 
-  console.log(islands);
-
-  // load islands
-  const lazyLoadingURLs =
-    "https://www.mujkaktus.cz/moje-sluzby" +
-    "?p_p_id=rkaktusvcc_WAR_vcc&p_p_lifecycle=0" +
-    "&p_p_state=exclusive&p_p_mode=view" +
-    "&p_p_col_id=column-1&p_p_col_count=1" +
-    "&_rkaktusvcc_WAR_vcc_moduleCode=dashboard" +
-    "&_rkaktusvcc_WAR_vcc_lazyLoading=true" +
-    "&_rkaktusvcc_WAR_vcc_componentIds=" +
-    islands.map(({ mainId, subId }) => [mainId, subId].join(".")).join("|");
-    
-  const islandsText = await (await fetch(lazyLoadingURLs)).text();
-
-  console.log(islandsText);
-  return islandsText;
+  return islands;
 }
 
 await checkMobileData();
